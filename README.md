fe136895102f93c01edb383d11606e302282d475
Leran-Johnson <leranjohnson420@gmail.com>
2023-11-02 18:57:39 -0400

    Added core files index,Product, and Support. As well as needed CSS and JS files and image folders

---
+++ README.md
@@ -0,0 +1,1 @@
+Created website with 3 pages A index that introduces the website a product page with 12 unique products and a feedback form with validation on the name and email portions.
---
+++ support.js
@@ -0,0 +1,28 @@
+function validateForm() {
+  // Get the name and email input values
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+
+  // Get the error message elements
+  var nameError = document.getElementById("name-error");
+  var emailError = document.getElementById("email-error");
+
+  // Check if the name is empty
+  if (name.trim() === "") {
+    nameError.innerHTML = "Name is required";
+    nameError.style.display = "block";
+    return false;
+  } else {
+    nameError.style.display = "none";
+  }
+
+  // Check if the email is empty
+  if (email.trim() === "") {
+    emailError.innerHTML = "Email is required";
+    emailError.style.display = "block";
+    return false; 
+  } else {
+    emailError.style.display = "none";
+  }
+  return true;
+}
